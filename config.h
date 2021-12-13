96bf100e45b0728ad23df2ff11268d79bf3d09a1
phat_sumo <ethanjpayne1@gmail.com>
2021-12-12 17:46:03 -0700

    set colors

--- config.h
+++ config.h
@@ -3,23 +3,24 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[BACKGROUND] =   "black", /* after initialization */
-	[INIT] =   "#2d2d2d",     /* after initialization */
-	[INPUT] =  "#005577",     /* during input */
-	[FAILED] = "#CC3333",     /* wrong password */
+	[BACKGROUND] =  "#090909", /* after initialization */
+	[INIT] =        "#030303", /* after initialization */
+	[INPUT] =       "#00fbc3", /* during input */
+	[FAILED] =      "#fe3198", /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "Welcome back! Staying long?";
 
 /* text color */
-static const char * text_color = "#ffffff";
+static const char * text_color = "#fbf5f3";
 
 /* font name (must be a valid size) */
 static const char * font_name = "-misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1";
+// static const char * font_name = "-*-monoid-light-r-*--12-*-100-100-p-0-iso8859-1";
 
 
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
@@ -28,7 +29,7 @@ static const int logow = 12;	/* grid width and height for right center alignment
 static const int logoh = 6;
 
 static XRectangle rectangles[9] = {
-	/* x	y	w	h */
+	/* x	y	  w	  h */
 	{ 0,	3,	1,	3 },
 	{ 1,	3,	2,	1 },
 	{ 0,	5,	8,	1 },
